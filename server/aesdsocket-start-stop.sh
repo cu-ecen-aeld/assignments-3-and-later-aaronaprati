f95481ed19cabf3a16b31a0da748c82215f70ac4
Aaron <aaron@Ubuntu.myguest.virtualbox.org>
2023-08-08 17:08:03 -0700

    Changes for assignment-5-part-2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to handle the start
+start() {
+  echo "Starting aesdsocket in daemon mode..."
+  ./aesdsocket -d
+}
+
+# Function to handle the stop
+stop() {
+  echo "Stopping aesdsocket..."
+  pkill -TERM 'aesdsocket'
+  # Perform any additional cleanup steps here
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
+
